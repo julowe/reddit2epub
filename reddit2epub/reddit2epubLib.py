b9694803c043c9d92146a7a2ce455f7e2f9190fd
julowe <10883713+julowe@users.noreply.github.com>
2024-02-22 18:21:31 -0800

    autoformatting applied to existing code

--- reddit2epub/reddit2epubLib.py
+++ reddit2epub/reddit2epubLib.py
@@ -5,65 +5,77 @@ from ebooklib import epub
 from ebooklib.epub import EpubBook
 from praw.reddit import Redditor, Submission, Subreddit
 
-reddit = praw.Reddit(client_id="sUBJ9ERh2RyjmQ", client_secret=None,
-                     user_agent='Reddit stories to epub by mircohaug')
+reddit = praw.Reddit(
+    client_id="sUBJ9ERh2RyjmQ",
+    client_secret=None,
+    user_agent="Reddit stories to epub by mircohaug",
+)
+
 
 def get_chapters_from_anchor(
-        input_url,
-        overlap: int = 2,
-        all_reddit: bool = False,
+    input_url,
+    overlap: int = 2,
+    all_reddit: bool = False,
 ) -> (Redditor, List[Submission], str):
     author, post_subreddit, title = process_anchor_url(input_url)
 
     search_title = " ".join(title.split(" ")[:overlap])
 
-    selected_submissions = get_selected_posts(author=author,
-                                              post_subreddit=post_subreddit,
-                                              all_reddit=all_reddit,
-                                              search_title=search_title)
+    selected_submissions = get_selected_posts(
+        author=author,
+        post_subreddit=post_subreddit,
+        all_reddit=all_reddit,
+        search_title=search_title,
+    )
     return author, selected_submissions, search_title
 
 
 def create_book_from_chapters(
-        book_author: str,
-        book_id: str,
-        book_title: str,
-        reddit_chapters: Iterable[Submission]) -> EpubBook:
+    book_author: str,
+    book_id: str,
+    book_title: str,
+    reddit_chapters: Iterable[Submission],
+) -> EpubBook:
     book = epub.EpubBook()
     book.set_identifier(book_id)
     book.set_title(book_title)
     book.add_author(book_author)
-    book.set_language('en')
-    cover = epub.EpubHtml(title=book_title, file_name='cover.xhtml', lang='en')
-    cover.content = "<div><h1>{0}</h1>" \
-                    "<h2><a href=\"https://www.reddit.com/user/{1}\">{1}</a></h2>" \
-                    "{2}</div>".format(book_title, book_author,
-                                       "Created with the reddit2epub python package")
+    book.set_language("en")
+    cover = epub.EpubHtml(title=book_title, file_name="cover.xhtml", lang="en")
+    cover.content = (
+        "<div><h1>{0}</h1>"
+        '<h2><a href="https://www.reddit.com/user/{1}">{1}</a></h2>'
+        "{2}</div>".format(
+            book_title, book_author, "Created with the reddit2epub python package"
+        )
+    )
     book.add_item(cover)
     book_chapters = []
     # check for title prefix
     for i, sub in enumerate(reddit_chapters):
         # create chapter
-        c1 = epub.EpubHtml(title=sub.title, file_name='chap_{}.xhtml'.format(i), lang='en')
+        c1 = epub.EpubHtml(
+            title=sub.title, file_name="chap_{}.xhtml".format(i), lang="en"
+        )
         c1.content = """<h1>{0}</h1>
                      <a href="{1}">Original</a>
                      {2}
                      <a href="{1}">Original</a>
-                     """.format(sub.title, sub.shortlink, sub.selftext_html)
+                     """.format(
+            sub.title, sub.shortlink, sub.selftext_html
+        )
 
         # add chapter
         book.add_item(c1)
         book_chapters.append(c1)
 
     # define Table Of Contents
-    book.toc = (
-        book_chapters
-    )
+    book.toc = book_chapters
     # add default NCX and Nav file
     book.add_item(epub.EpubNcx())
     book.add_item(epub.EpubNav())
     # basic spine
-    spine = [cover, 'nav']
+    spine = [cover, "nav"]
     spine.extend(book_chapters)
     # is used to generate the toc at the start
     book.spine = spine
@@ -71,18 +83,19 @@ def create_book_from_chapters(
 
 
 def get_selected_posts(
-        author: Redditor,
-        post_subreddit: Subreddit,
-        search_title: str,
-        all_reddit: bool = False,
+    author: Redditor,
+    post_subreddit: Subreddit,
+    search_title: str,
+    all_reddit: bool = False,
 ) -> List[Submission]:
     if all_reddit:
-        sub_to_search_in = reddit.subreddit('all')
+        sub_to_search_in = reddit.subreddit("all")
     else:
         sub_to_search_in = post_subreddit
     # is limited to 250 items
-    list_of_posts = sub_to_search_in.search("author:\"{}\" title:\"{}\" ".format(author, search_title), limit=None,
-                                            sort='new')
+    list_of_posts = sub_to_search_in.search(
+        'author:"{}" title:"{}" '.format(author, search_title), limit=None, sort="new"
+    )
     list_of_posts = list(list_of_posts)
     selected_submissions = []
     for p in list_of_posts:
--- setup.py
+++ setup.py
@@ -5,22 +5,22 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='reddit2epub',
-    version='0.5.0',
-    description='A CLI to convert reddit series into epub files',
+    name="reddit2epub",
+    version="0.5.0",
+    description="A CLI to convert reddit series into epub files",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/mircohaug/reddit2epub.git',
-    author='Mirco Haug',
-    author_email='python@mircohaug.de',
+    url="https://github.com/mircohaug/reddit2epub.git",
+    author="Mirco Haug",
+    author_email="python@mircohaug.de",
     packages=setuptools.find_packages(),
     install_requires=[
-        'click==7.*',
-        'praw==6.*',
-        'EbookLib==0.17.*',
+        "click==7.*",
+        "praw==6.*",
+        "EbookLib==0.17.*",
     ],
     entry_points={
-        "console_scripts": ['reddit2epub = reddit2epub.reddit2epubCli:main_cli']
+        "console_scripts": ["reddit2epub = reddit2epub.reddit2epubCli:main_cli"]
     },
     # from https://pypi.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -29,6 +29,6 @@ setup(
         "Intended Audience :: End Users/Desktop",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Topic :: Utilities"
+        "Topic :: Utilities",
     ],
 )
